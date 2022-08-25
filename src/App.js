e234c0f7688ab54222f2a50aa88fc687da646b36
JohnnyNWT <jooaaooskt@gmail.com>
2022-08-25 19:24:37 -0300

    Req - 5

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends React.Component {
     cardImage: '',
     cardRare: '',
     cardTrunfo: false,
+    isSaveButtonDisabled: true,
   };
 
   handleChange = ({ target }) => {
@@ -20,6 +21,34 @@ class App extends React.Component {
 
     this.setState({
       [name]: value,
+    }, () => {
+      this.handleButton();
+    });
+  };
+
+  handleButton = () => {
+    const {
+      cardName,
+      cardDescription,
+      cardImage,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+    } = this.state;
+
+    const noEmpty = cardName.length > 0
+    && cardDescription.length > 0 && cardImage.length > 0;
+    const maxPoints = 90;
+    const totalMaxPoints = 210;
+    const sumAttrs = Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3);
+    const validateSum = sumAttrs <= totalMaxPoints
+    && Number(cardAttr1) >= 0 && Number(cardAttr1) <= maxPoints
+    && Number(cardAttr2) >= 0 && Number(cardAttr2) <= maxPoints
+    && Number(cardAttr3) >= 0 && Number(cardAttr3) <= maxPoints;
+    const enableButton = validateSum === true && noEmpty === true;
+
+    this.setState({
+      isSaveButtonDisabled: !enableButton,
     });
   };
 
@@ -33,12 +62,14 @@ class App extends React.Component {
       cardImage,
       cardRare,
       cardTrunfo,
+      isSaveButtonDisabled,
     } = this.state;
     return (
       <div>
         <h1>Adicionar nova carta</h1>
         <Form
           onInputChange={ this.handleChange }
+          isSaveButtonDisabled={ isSaveButtonDisabled }
           cardName={ cardName }
           cardDescription={ cardDescription }
           cardAttr1={ cardAttr1 }
